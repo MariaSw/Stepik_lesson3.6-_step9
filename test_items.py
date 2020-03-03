98edb26e0caf96d1bac4b7b93e1cf4e97fd1dfd9
MariaSw <kryvunmaria@gmail.com>
2020-03-03 17:09:21 +0300

    finished code

---
+++ test_items.py
@@ -0,0 +1,19 @@
+import pytest
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+import time
+from selenium.webdriver.common.by import By
+
+
+@pytest.mark.parametrize('link', [
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"])
+class TestBookStore():
+    def test_button(self, browser, link):
+        browser.implicitly_wait(5)
+        browser.get(f"{link}")
+        time.sleep(10)
+        try:
+            browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
+        except NoSuchElementException:
+            assert False, "Add to card Button is absent"
+
